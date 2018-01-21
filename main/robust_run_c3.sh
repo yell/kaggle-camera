eaaa91c71bf994d7a866ac6e44e5c33dc2ae3adf
monsta <yell.bondarenko@gmail.com>
2018-01-21 23:53:05 +0100

    upd paths[2]

--- main/robust_run_c3.sh
+++ main/robust_run_c3.sh
@@ -3,6 +3,6 @@ n_restarts='12';
 [[ -z $1 ]] || n_restarts=$1
 for i in `seq 1 ${n_restarts}`; 
 do
-    python run.py --epochs 150 --resume-from models/c3/
+    python run.py --epochs 150 --resume-from c3/
     sleep 5;
 done
--- main/robust_run_r34-2.sh
+++ main/robust_run_r34-2.sh
@@ -3,6 +3,6 @@ n_restarts='12';
 [[ -z $1 ]] || n_restarts=$1
 for i in `seq 1 ${n_restarts}`; 
 do
-    python run_pretrained.py --model resnet34 --batch-size 64 --epochs 150 --resume-from models/r34-2/
+    python run_pretrained.py --model resnet34 --batch-size 64 --epochs 150 --resume-from r34-2/
     sleep 5;
 done
