12a3b42cbbfa239d6870988d35884153b1773d1b
monsta <yell.bondarenko@gmail.com>
2018-01-24 01:24:17 +0100

    restructure CNN3

--- main/run.py
+++ main/run.py
@@ -71,26 +71,26 @@ class CNN3(nn.Module):
     def __init__(self, num_classes=10):
         super(CNN3, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=9, stride=1),
-            nn.BatchNorm2d(num_features=128),
-            nn.PReLU(),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=7, stride=1),
+            nn.Conv2d(in_channels=64, out_channels=96, kernel_size=7, stride=1),
             nn.BatchNorm2d(num_features=96),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=128, out_channels=160, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=5, stride=1),
             nn.BatchNorm2d(num_features=128),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=4, stride=4),
-            nn.Conv2d(in_channels=160, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
             nn.BatchNorm2d(num_features=256),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=4, stride=4),
         )
         self.classifier = nn.Sequential(
             nn.Linear(1024, 128),
-            nn.PReLU(),
+            nn.ReLU(inplace=True),
             nn.Linear(128, num_classes),
         )
         for layer in self.modules():
