aad12d90b15b3a80ce08e5bc1bc0afc5bff39def
monsta <yell.bondarenko@gmail.com>
2018-01-24 22:01:09 +0100

    TMP: comp val every epoch

--- main/run.py
+++ main/run.py
@@ -435,7 +435,7 @@ def main(**kwargs):
                                         optim=torch.optim.Adam, optim_params=dict(lr=kwargs['lr']),
                                         loss_func={'logloss': nn.CrossEntropyLoss,
                                                    'hinge': nn.MultiMarginLoss}[kwargs['loss']](),
-                                        max_epoch=0, val_each_epoch=kwargs['epochs_per_unique_data'],
+                                        max_epoch=0, val_each_epoch=1,#kwargs['epochs_per_unique_data'],
                                         path_template=path_template)
 
     if kwargs['predict_from']:
