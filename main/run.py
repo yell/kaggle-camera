77b41a0e32d866c47576e684e0a7a7c1d5889e3e
monsta <yell.bondarenko@gmail.com>
2018-01-28 18:08:56 +0100

    kwargs -> args + minor cleanup

--- main/run.py
+++ main/run.py
@@ -43,7 +43,7 @@ parser.add_argument('--batch-size', type=int, default=64,
                     help='input batch size for training')
 parser.add_argument('--optim', type=str, default='sgd',
                     help="optimizer, {'adam', 'sgd'}")
-parser.add_argument('--lr', type=float, default=[1e-4, 1e-3], nargs='+',
+parser.add_argument('--lr', type=float, default=[1e-3], nargs='+',
                     help='initial learning rate(s)')
 parser.add_argument('--cyclic-lr', type=float, default=None, nargs='+',
                     help='cyclic LR in form (lr-min, lr-max, stepsize)')
@@ -51,9 +51,9 @@ parser.add_argument('--epochs', type=int, default=300,
                     help='number of epochs')
 parser.add_argument('--epochs-per-unique-data', type=int, default=8,
                     help='number of epochs run per unique subset of data')
-parser.add_argument('--lrm', type=float, default=1., nargs='+',
+parser.add_argument('--lrm', type=float, default=[1.], nargs='+',
                     help='learning rates multiplier, used only when resume training')
-parser.add_argument('--model-dirpath', type=str, default='models/',
+parser.add_argument('--model-dirpath', type=str, default='../models/',
                     help='directory path to save the model and predictions')
 parser.add_argument('--means', type=float, default=(0.485, 0.456, 0.406), nargs='+',
                     help='per-channel means to use in preprocessing')
@@ -74,7 +74,13 @@ parser.add_argument('--optical', action='store_true',
 args = parser.parse_args()
 args.means = list(args.means)
 args.stds = list(args.stds)
-kwargs = dict(**vars(args))
+if len(args.lr) == 1:
+    args.lr *= 2
+if len(args.lrm) == 1:
+    args.lrm *= 2
+args.model = args.model.lower()
+args.loss = args.loss.lower()
+args.optim = args.optim.lower()
 
 
 K = 1/12. * np.array([[-1,  2,  -2,  2, -1],
@@ -137,27 +143,26 @@ def make_train_loaders(folds):
     y_train = []
     X_train = []
     for fold_id in folds:
-        y_train += np.load(os.path.join(kwargs['data_path'], 'y_{0}.npy'.format(fold_id))).tolist()
-        X_fold   = np.load(os.path.join(kwargs['data_path'], 'X_{0}.npy'.format(fold_id)))
+        y_train += np.load(os.path.join(args.data_path, 'y_{0}.npy'.format(fold_id))).tolist()
+        X_fold   = np.load(os.path.join(args.data_path, 'X_{0}.npy'.format(fold_id)))
         X_train += [X_fold[i] for i in xrange(len(X_fold))]
-    X_pseudo = np.load(os.path.join(kwargs['data_path'], 'X_pseudo_train.npy'))
+    X_pseudo = np.load(os.path.join(args.data_path, 'X_pseudo_train.npy'))
     ind = [5*fold_id + i for i in xrange(5) for fold_id in folds]
     X_train += [X_pseudo[i] for i in ind]
 
     # make dataset
     rng = RNG()
-    # noinspection PyTypeChecker
     train_transforms_list = [
         transforms.Lambda(lambda x: Image.fromarray(x))
     ]
-    if kwargs['optical']:
+    if args.optical:
         train_transforms_list += [
-            transforms.Lambda(lambda img: random_optical_crop(img, rng, kwargs['crop_size'])),
+            transforms.Lambda(lambda img: random_optical_crop(img, rng, args.crop_size)),
             # transforms.Lambda(lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_180) if rng.rand() < 0.5 else img),
         ]
     else:
         train_transforms_list += [
-            transforms.RandomCrop(kwargs['crop_size']),
+            transforms.RandomCrop(args.crop_size),
             transforms.RandomHorizontalFlip(),
             # transforms.RandomVerticalFlip(),
             # transforms.Lambda(lambda img: [img,
@@ -167,7 +172,7 @@ def make_train_loaders(folds):
         transforms.Lambda(lambda img: adjust_gamma(img, gamma=rng.choice([0.8, 1.0, 1.2]))),
         transforms.Lambda(lambda img: jpg_compress(img, quality=rng.choice([70, 90, 100]))),
     ]
-    if kwargs['kernel']:
+    if args.kernel:
         train_transforms_list += [
             transforms.Lambda(lambda img: conv_K(np.asarray(img, dtype=np.uint8))),
             transforms.Lambda(lambda x: torch.from_numpy(x.transpose(2, 0, 1)))
@@ -176,18 +181,18 @@ def make_train_loaders(folds):
         train_transforms_list += [
             transforms.ToTensor(),
         ]
-    train_transforms_list += [transforms.Normalize(kwargs['means'], kwargs['stds'])]
+    train_transforms_list += [transforms.Normalize(args.means, args.stds)]
     train_transform = transforms.Compose(train_transforms_list)
     dataset = make_numpy_dataset(X=X_train, y=y_train,
                                  transform=train_transform)
 
     # make loader
     loader = DataLoader(dataset=dataset,
-                        batch_size=kwargs['batch_size'],
+                        batch_size=args.batch_size,
                         shuffle=False,
-                        num_workers=kwargs['n_workers'],
+                        num_workers=args.n_workers,
                         sampler=StratifiedSampler(class_vector=np.asarray(y_train),
-                                                  batch_size=kwargs['batch_size']))
+                                                  batch_size=args.batch_size))
     return loader
 
 def train_optimizer(optimizer, train_loader, val_loader):
@@ -200,7 +205,7 @@ def train_optimizer_pretrained(optimizer, train_loader, val_loader):
             param.requires_grad = False
 
         max_epoch = optimizer.max_epoch
-        optimizer.max_epoch = optimizer.epoch + kwargs['epochs_per_unique_data']
+        optimizer.max_epoch = optimizer.epoch + args.epochs_per_unique_data
         optimizer.train(train_loader, val_loader)
 
         # now unfreeze features
@@ -213,36 +218,36 @@ def train_optimizer_pretrained(optimizer, train_loader, val_loader):
 def train(optimizer, train_optimizer=train_optimizer):
     # load and crop validation data
     print "Loading data ..."
-    X_val = np.load(os.path.join(kwargs['data_path'], 'X_val.npy'))
-    y_val = np.load(os.path.join(kwargs['data_path'], 'y_val.npy')).tolist()
-    c = kwargs['crop_size']
+    X_val = np.load(os.path.join(args.data_path, 'X_val.npy'))
+    y_val = np.load(os.path.join(args.data_path, 'y_val.npy')).tolist()
+    c = args.crop_size
     C = X_val.shape[1]
     if c < C:
         X_val = X_val[:, C/2-c/2:C/2+c/2, C/2-c/2:C/2+c/2, :]
     X_val = [X_val[i] for i in xrange(len(X_val))]
-    if kwargs['kernel']:
+    if args.kernel:
         X_val = [conv_K(x) for x in X_val]
 
     # compute folds numbers
-    fold = kwargs['fold']
+    fold = args.fold
     N_folds = 100
     val_folds = [2*fold, 2*fold + 1]
     val_folds.append('pseudo_val')
     train_folds = range(N_folds)[:2*fold] + range(N_folds)[2*fold + 2:]
     G = cycle(train_folds)
-    for _ in xrange(kwargs['skip_train_folds']):
+    for _ in xrange(args.skip_train_folds):
         next(G)
 
     # load val data
     for fold_id in val_folds:
-        X_fold = np.load(os.path.join(kwargs['data_path'], 'X_{0}.npy'.format(fold_id)))
+        X_fold = np.load(os.path.join(args.data_path, 'X_{0}.npy'.format(fold_id)))
         D = X_fold.shape[1]
         X_fold = X_fold[:, D/2-c/2:D/2+c/2, D/2-c/2:D/2+c/2, :]
         Z = [X_fold[i] for i in xrange(len(X_fold))]
-        if kwargs['kernel']:
+        if args.kernel:
             Z = [conv_K(x) for x in Z]
         X_val += Z
-        y_fold = np.load(os.path.join(kwargs['data_path'], 'y_{0}.npy'.format(fold_id))).tolist()
+        y_fold = np.load(os.path.join(args.data_path, 'y_{0}.npy'.format(fold_id))).tolist()
         y_val += y_fold
 
     # make validation loader
@@ -250,62 +255,60 @@ def train(optimizer, train_optimizer=train_optimizer):
         transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.ToTensor(),
         # transforms.Lambda(lambda x: torch.from_numpy(x.transpose(2, 0, 1))),
-        transforms.Normalize(kwargs['means'], kwargs['stds'])
+        transforms.Normalize(args.means, args.stds)
     ])
     val_dataset = make_numpy_dataset(X=X_val,
                                      y=y_val,
                                      transform=val_transform)
     val_loader = DataLoader(dataset=val_dataset,
-                            batch_size=kwargs['batch_size'],
+                            batch_size=args.batch_size,
                             shuffle=False,
-                            num_workers=kwargs['n_workers'])
+                            num_workers=args.n_workers)
 
-    n_runs = kwargs['epochs'] / kwargs['epochs_per_unique_data'] + 1
+    n_runs = args.epochs / args.epochs_per_unique_data + 1
 
-    print "Starting training ..."
     for _ in xrange(n_runs):
         current_folds = []
-        for j in xrange(kwargs['n_train_folds']):
+        for j in xrange(args.n_train_folds):
             current_folds.append(next(G))
 
         train_loader = make_train_loaders(folds=current_folds)
 
-        optimizer.max_epoch = optimizer.epoch + kwargs['epochs_per_unique_data']
+        optimizer.max_epoch = optimizer.epoch + args.epochs_per_unique_data
         train_optimizer(optimizer, train_loader, val_loader)
 
 def make_test_dataset_loader():
     test_transform = transforms.Compose([
-        transforms.CenterCrop(kwargs['crop_size']),
+        transforms.CenterCrop(args.crop_size),
         transforms.ToTensor(),
-        transforms.Normalize(kwargs['means'], kwargs['stds'])
+        transforms.Normalize(args.means, args.stds)
     ])
 
     # TTA
     rng = RNG()
-    base_transforms = []
-    if kwargs['optical']:
-        base_transforms += [
-            transforms.Lambda(lambda img: random_optical_crop(img, rng, kwargs['crop_size'])),
+    base_transforms_list = []
+    if args.optical:
+        base_transforms_list += [
+            transforms.Lambda(lambda img: random_optical_crop(img, rng, args.crop_size)),
             # transforms.Lambda(lambda img: img.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_180) if rng.rand() < 0.5 else img),
         ]
     else:
-        base_transforms += [
-            transforms.RandomCrop(kwargs['crop_size']),
+        base_transforms_list += [
+            transforms.RandomCrop(args.crop_size),
             # transforms.RandomHorizontalFlip(),
             # transforms.RandomVerticalFlip(),
             # transforms.Lambda(lambda img: [img,
             #                                img.transpose(Image.ROTATE_90)][int(rng.rand() < 0.5)]),
         ]
-    base_transforms += [
+    base_transforms_list += [
         transforms.Lambda(lambda img: adjust_gamma(img, gamma=rng.choice([0.8, 1.0, 1.2]))),
         # transforms.Lambda(lambda img: jpg_compress(img, quality=rng.choice([70, 90, 100]))),
         transforms.ToTensor(),
-        transforms.Normalize(kwargs['means'], kwargs['stds'])
+        transforms.Normalize(args.means, args.stds)
     ]
-    base_transform = transforms.Compose(*base_transforms)
+    base_transform = transforms.Compose(*base_transforms_list)
 
-    tta_n = kwargs['tta_n']
-    def tta_f(img, n=tta_n - 1):
+    def tta_f(img, n=args.tta_n - 1):
         out = [test_transform(img)]
         for _ in xrange(n):
             out.append(base_transform(img))
@@ -315,12 +318,12 @@ def make_test_dataset_loader():
         transforms.Lambda(lambda img: tta_f(img)),
     ])
 
-    test_dataset = KaggleCameraDataset(kwargs['data_path'], train=False,
+    test_dataset = KaggleCameraDataset(args.data_path, train=False,
                                        transform=tta_transform)
     test_loader = DataLoader(dataset=test_dataset,
-                             batch_size=kwargs['batch_size'],
+                             batch_size=args.batch_size,
                              shuffle=False,
-                             num_workers=kwargs['n_workers'])
+                             num_workers=args.n_workers)
     return test_dataset, test_loader
 
 def predict(optimizer):
@@ -351,7 +354,7 @@ def predict(optimizer):
     array([[ 0.29872292,  0.32929052,  0.37198656],
            [ 0.22675917,  0.31754289,  0.45569794]])
     """
-    tta_n = kwargs['tta_n']
+    tta_n = args.tta_n
     logits = logits.reshape(len(logits) / tta_n, tta_n, -1).mean(axis=1)
 
     proba = softmax(logits)
@@ -361,7 +364,7 @@ def predict(optimizer):
     df = pd.DataFrame(proba)
     df['fname'] = fnames
     df = df[['fname'] + range(10)]
-    dirpath = os.path.split(kwargs['predict_from'])[0]
+    dirpath = os.path.split(args.predict_from)[0]
     df.to_csv(os.path.join(dirpath, 'proba.csv'), index=False)
 
     # compute predictions and save in submission format
@@ -373,50 +376,52 @@ def predict(optimizer):
 
 def main():
     # build model
-    if not kwargs['model_dirpath'].endswith('/'):
-        kwargs['model_dirpath'] += '/'
+    if not args.model_dirpath.endswith('/'):
+        args.model_dirpath += '/'
 
     print 'Building model ...'
-    model, is_pretrained = get_model(kwargs['model'])
-    model = model(input_size=kwargs['crop_size'])
+    model_cls, is_pretrained = get_model(args.model)
+    model = model_cls(input_size=args.crop_size)
 
     model_params = [
-        {'params': model.features.parameters(),
-         'lr': kwargs['lr'][0]},
-        {'params': model.classifier.parameters(),
-         'lr': kwargs['lr'][min(1, len(kwargs['lr']) - 1)],
-         'weight_decay': 1e-5},
+        {'params': model.features.parameters(), 'lr': args.lr[0]},
+        {'params': model.classifier.parameters(), 'lr': args.lr[1], 'weight_decay': 1e-5},
     ]
 
     optim = {'adam': torch.optim.Adam,
-              'sgd': torch.optim.SGD}[kwargs['optim']]
-    optim_params = {'lr': kwargs['lr']}
-    if kwargs['optim'] == 'sgd':
+              'sgd': torch.optim.SGD}[args.optim]
+    optim_params = {'lr': args.lr[0]}
+    if args.optim == 'sgd':
         optim_params['momentum'] = 0.9
 
-    path_template = os.path.join(kwargs['model_dirpath'], '{acc:.4f}-{epoch}')
+    path_template = os.path.join(args.model_dirpath, '{acc:.4f}-{epoch}')
     optimizer = ClassificationOptimizer(model=model, model_params=model_params,
                                         optim=optim, optim_params=optim_params,
                                         loss_func={'logloss': nn.CrossEntropyLoss,
-                                                   'hinge': nn.MultiMarginLoss}[kwargs['loss']](),
-                                        max_epoch=0, val_each_epoch=kwargs['epochs_per_unique_data'],
-                                        cyclic_lr=kwargs['cyclic_lr'],
+                                                   'hinge': nn.MultiMarginLoss}[args.loss](),
+                                        max_epoch=0, val_each_epoch=args.epochs_per_unique_data,
+                                        cyclic_lr=args.cyclic_lr,
                                         path_template=path_template)
 
-    if kwargs['predict_from']:
-        optimizer.load(kwargs['predict_from'])
+    if args.predict_from:
+        if not args.predict_from.endswith('ckpt') and not args.predict_from.endswith('/'):
+            args.predict_from += '/'
+        print 'Predicting from checkpoint ...'
+        optimizer.load(args.predict_from)
         predict(optimizer)
         return
 
-    if kwargs['resume_from']:
-        if not kwargs['resume_from'].endswith('ckpt') and not kwargs['resume_from'].endswith('/'):
-            kwargs['resume_from'] += '/'
+    if args.resume_from:
+        if not args.resume_from.endswith('ckpt') and not args.resume_from.endswith('/'):
+            args.resume_from += '/'
         print 'Resuming from checkpoint ...'
-        optimizer.load(kwargs['resume_from'])
-        optimizer.path_template = os.path.join(*(list(os.path.split(kwargs['resume_from'])[:-1]) + ['{acc:.4f}-{epoch}']))
-        optimizer._mul_lr_by(kwargs['lrm'])
+        optimizer.load(args.resume_from)
+        optimizer.path_template = os.path.join(*(list(os.path.split(args.resume_from)[:-1]) + ['{acc:.4f}-{epoch}']))
+        optimizer._mul_lr_by(args.lrm)
+    else:
+        print 'Starting training ...'
 
-    optimizer.max_epoch = optimizer.epoch + kwargs['epochs']
+    optimizer.max_epoch = optimizer.epoch + args.epochs
     train(optimizer,
           train_optimizer=train_optimizer_pretrained if is_pretrained else train_optimizer)
 
