045dccbddd37da3b5ace6b001b1a3f966e91302c
monsta <yell.bondarenko@gmail.com>
2018-02-06 16:19:25 +0100

    minor changes

--- main/gen_bagged_runs.py
+++ main/gen_bagged_runs.py
@@ -1,25 +1,28 @@
 import env
 from utils import RNG
 
-b = ['-b 16', '-b 12', '-b 8']
-cs = ['-cs 256', '-cs 256', '-cs 224', '-cs 224', '-cs 192']
-d = ['-d 0', '-d 0.05', '-d 0.1', '-d 0.1', '-d 0.15']
-eu = ['-eu 8', '-eu 8', '-eu 8', '-eu 6', '-eu 6', '-eu 4']
-fc = ['', '-fc 1024 256']
-l = ['-l logloss', '-l logloss', '-l logloss', '-l logloss', '-l hinge']
-opt = ['', '', '', '', '-opt adam']
-w = ['-w', '-w', '-w', '']
-nclr = [50, 50, 50, 80, 100]
+a = [''] * 2 + ['-a']
+b = ['-b 16'] * 3 + ['-b 12'] * 3 + ['-b 8']
+cs = ['-cs 256'] * 2 + ['-cs 224']
+d = ['-d 0', '-d 0.05', '-d 0.1', '-d 0.1', '-d 0.15'] # TODO
+eu = ['-eu 8'] * 3 + ['-eu 6'] * 2 + ['-eu 4']
+fc = [''] * 3 + ['-fc 1024 256'] * 2
+l_lr = ['-lr 1e-4 1e-3 -clr 1e-3 4e-3'] * 5 + ['-l hinge -lr 1e-5 1e-4 -clr 1e-4 4e-4']
+opt = [''] * 5 + ['-opt adam']
+w = ['-w'] * 4 + ['']
+nclr = [50] * 2 + [80] + [100] * 2
+T = ['-t 0'] + ['-t 4'] * 5 + ['-t 6'] * 3 + ['-t 8'] * 2
+dc = ['-dc 0.04', '-dc 0.08', '-dc 0.1']
 
 seed = 111
 
 rng = RNG(1337)
 for i in xrange(100):
-    tmpl = 'nohup python run.py {b} {cs} {d} {eu} {fc} {l} {opt} {w} -e 4000 -bt -rs {seed}'
-    tmpl += ' -lr 1e-4 1e-3 -clr 1e-3 4e-3 {nclr}'
+    tmpl = 'nohup python run.py {a} {b} {cs} {d} {eu} {fc} {opt} {w} -e 4000 -bt -rs {seed}'
+    tmpl += ' {l_lr} {nclr} {T} {dc}'
     tmpl += ' -md ../models/{path}/ > {seed}.out &'
     current = {}
-    for v in ('b', 'cs', 'd', 'eu', 'fc', 'l', 'opt', 'w', 'nclr'):
+    for v in ('a', 'b', 'cs', 'd', 'eu', 'fc', 'opt', 'w', 'l_lr', 'nclr', 'T', 'dc'):
         current[v] = rng.choice(globals()[v])
     path = 'd121-bagg-{i}'.format(i=i + 1)
     # path = 'b{b}_cs{cs}_d{d}_eu{eu}_{fc}_{l}_{opt}_{w}_nclr{nclr}_seed{seed}'
